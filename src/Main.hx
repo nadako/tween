a840ac88d8b0dd873f4cd74270d3ea9791c2be8b
Dan Korostelev <nadako@gmail.com>
2016-06-15 05:34:22 +0300

    pass normalized time to the set function, rework visualization

--- src/Main.hx
+++ src/Main.hx
@@ -2,6 +2,9 @@ import kha.Framebuffer;
 import kha.Scheduler;
 import kha.System;
 import kha.input.Mouse;
+import kha.math.Vector2i;
+
+import easing.*;
 
 class Main {
 	public static function main() {
@@ -10,22 +13,35 @@ class Main {
 
 	var lastTime:Float;
 	var tween:Tween;
-	var x:Int;
+	var points:Array<Vector2i>;
+	var easingIndex:Int;
+	static var easings:Array<{function easeInOut(p:Float):Float;}> = [Back, Bounce, Circ, Cube, Elastic, Expo, Quad, Quart, Quint, Sine]; 
 	
-	static inline var SIZE = 30;
+	static inline var SIZE = 10;
 	static inline var MARGIN = 100;
 
 	function new() {
 		System.notifyOnRender(render);
 		Scheduler.addTimeTask(update, 0, 1 / 60);
 		Mouse.get().notify(onMouseDown, null, null, null);
-		x = MARGIN;
+		easingIndex = 0;
 		lastTime = Scheduler.time();
 	}
 	
 	function onMouseDown(_, _, _) {
-		x = MARGIN;
-		tween = new Tween(x, System.windowWidth() - SIZE - MARGIN, 1, easing.Quad.easeInOut, function(v) x = Std.int(v));
+		var ww = System.windowWidth() - MARGIN * 2 - SIZE;
+		var wh = System.windowHeight() - MARGIN * 2 - SIZE;
+		var idx = easingIndex++;
+		if (easingIndex >= easings.length)
+			easingIndex = 0;
+		var easing = easings[idx];
+		trace(Type.getClassName(cast easing));
+		points = [new Vector2i(MARGIN, wh)];
+		tween = new Tween(wh, MARGIN, 1, easing.easeInOut, function(t, v) {
+			var x = Std.int(MARGIN + ww * t); 
+			var y = Std.int(v);
+			points.push(new Vector2i(x, y));
+		});
 	}
 
 	function update(): Void {
@@ -37,10 +53,14 @@ class Main {
 	}
 
 	function render(framebuffer: Framebuffer): Void {
+		if (points == null)
+			return;
 		var g = framebuffer.g2;
 		g.begin();
 		g.color = kha.Color.White;
-		g.fillRect(x, (System.windowHeight() - SIZE) / 2, SIZE, SIZE);
+		for (point in points) {
+			g.fillRect(point.x, point.y, SIZE, SIZE);
+		}
 		g.end();				
 	}
 }
--- src/Tween.hx
+++ src/Tween.hx
@@ -3,11 +3,11 @@ class Tween {
 	var delta:Float;
 	var time:Float;
 	var duration:Float;
+	var set:Float->Float->Void;
 	var ease:Float->Float;
-	var set:Float->Void;
 	var finished:Bool;
 	
-	public function new(from:Float, to:Float, duration:Float, ease:Float->Float, set:Float->Void) {
+	public function new(from:Float, to:Float, duration:Float, ease:Float->Float, set:Float->Float->Void) {
 		this.start = from;
 		this.delta = to - from;
 		this.duration = duration;
@@ -23,10 +23,11 @@ class Tween {
 			
 		time += dt;
 		if (time >= duration) {
-			set(start + delta);
+			set(1, start + delta);
 			finished = true;
 		} else {
-			set(start + ease(time / duration) * delta);
+			var t = time / duration;
+			set(t, start + ease(t) * delta);
 		}
 	} 
 }
